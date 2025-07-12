96c40025eda71be8518c4c2fb1744e1a946bae4e
nazqiasalsabila <nazqiasalsabila@gmail.com>
2025-07-12 09:20:12 +0700

    Update app.py

--- app.py
+++ app.py
@@ -14,3 +14,15 @@ st.markdown("*Streamlit* is **really** ***cool***.")
 st.metric(label="Temperature", value="70 °F")
 
 st.link_button("Go to gallery", "https://streamlit.io/gallery")
+
+with col1:
+    st.header("A cat")
+    st.image("https://static.streamlit.io/examples/cat.jpg")
+
+with col2:
+    st.header("A dog")
+    st.image("https://static.streamlit.io/examples/dog.jpg")
+
+with col3:
+    st.header("An owl")
+    st.image("https://static.streamlit.io/examples/owl.jpg")
