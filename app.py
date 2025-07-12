5e45f00ccb0fb14f77c8c5eb735c510356fe699e
nazqiasalsabila <nazqiasalsabila@gmail.com>
2025-07-12 09:27:39 +0700

    Update app.py

--- app.py
+++ app.py
@@ -15,14 +15,4 @@ st.metric(label="Temperature", value="70 °F")
 
 st.link_button("Go to gallery", "https://streamlit.io/gallery")
 
-with col1:
-    st.header("A cat")
-    st.image("https://static.streamlit.io/examples/cat.jpg")
-
-with col2:
-    st.header("A dog")
-    st.image("https://static.streamlit.io/examples/dog.jpg")
-
-with col3:
-    st.header("An owl")
-    st.image("https://static.streamlit.io/examples/owl.jpg")
+st.success('This is a success message!', icon="✅")
