4780567ba09e0e7b26ebe128c4b1a554cacf6a7f
nazqiasalsabila <nazqiasalsabila@gmail.com>
2025-07-12 09:01:44 +0700

    Update app.py

--- app.py
+++ app.py
@@ -12,3 +12,5 @@ st.header("_Streamlit_ is :blue[cool] :sunglasses:divider="gray")
 st.subheader("_Streamlit_ is :blue[cool] :sunglasses:")
 
 st.markdown("*Streamlit* is **really** ***cool***.")
+
+st.metric(label="Temperature", value="70 °F")
