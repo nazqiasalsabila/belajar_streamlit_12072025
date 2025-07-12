25a2743491b081943ffa45ef69c9b434d10c2dc4
nazqiasalsabila <nazqiasalsabila@gmail.com>
2025-07-12 08:49:54 +0700

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import streamlit as st
 st.title("This is a title")
 st.title("_Streamlit_ is :blue[cool] :sunglasses:")
 
-st.header("_Streamlit_ is :blue[cool] :sunglasses:")
+st.header("_Streamlit_ is :blue[cool] :sunglasses:divider="gray")
 
 st.subheader("_Streamlit_ is :blue[cool] :sunglasses:")
 
