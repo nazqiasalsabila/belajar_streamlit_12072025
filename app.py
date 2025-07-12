822c916c26378c7ca77284e902c3c0249cf05495
nazqiasalsabila <nazqiasalsabila@gmail.com>
2025-07-12 09:31:29 +0700

    Update app.py

--- app.py
+++ app.py
@@ -15,4 +15,12 @@ st.metric(label="Temperature", value="70 °F")
 
 st.link_button("Go to gallery", "https://streamlit.io/gallery")
 
+if not st.user.is_logged_in:
+    if st.button("Log in"):
+        st.login()
+else:
+    if st.button("Log out"):
+        st.logout()
+    st.write(f"Hello, {st.user.name}!")
+
 st.success('This is a success message!', icon="✅")
